abfc4bb4a866b9eef646862c93abc4b91bbe2a39
Alexander Paz <alexjpaz@users.noreply.github.com>
2017-06-27 17:01:41 -0400

    Delete mrclean.js

--- scripts/mrclean.js
+++
@@ -1,5 +0,0 @@
-module.exports = (robot) => {
-  robot.hear(/mrclean/, (msg) => {
-    msg.send(':mrclean00::mrclean01::mrclean02:\n:mrclean10::mrclean11::mrclean12:\n:mrclean20::mrclean21::mrclean22:');
-  });
-};
